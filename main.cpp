6eab2fd9c6eebeb2a7acbcc75750301fef072241
Ivan Rybalko <ivribalko@gmail.com>
2021-01-09 22:02:12 -0500

    create randomized array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getRandomNumbers(int count)
+{
+    vector<int> numbers;
+
+    for (int i = 0; i < count; i++)
+    {
+        numbers.push_back(i);
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        swap(numbers[i], numbers[rand() % count]);
+    }
+
+    return numbers;
+}
 
 int main()
 {
-    std::cout << "Hello!" << std::endl;
+    vector<int> numbers = getRandomNumbers(1000);
 }
