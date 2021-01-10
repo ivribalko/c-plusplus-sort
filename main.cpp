5f2b7a24f3909b28a26befcdf1f84927fa9016f8
Ivan Rybalko <ivribalko@gmail.com>
2021-01-10 12:39:46 -0500

    insertion sort

--- main.cpp
+++ main.cpp
@@ -4,18 +4,20 @@
 
 using namespace std;
 
-vector<int> getUnsortedArray(int count)
+vector<int> getUnsortedArray(int size)
 {
+    srand(time(NULL));
+
     vector<int> array;
 
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < size; i++)
     {
         array.push_back(i);
     }
 
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < size; i++)
     {
-        swap(array[i], array[rand() % count]);
+        swap(array[i], array[rand() % size]);
     }
 
     return array;
@@ -43,18 +45,53 @@ vector<int> bubbleSort(vector<int> array)
     return array;
 }
 
-int main()
+vector<int> insertionSort(vector<int> array)
 {
-    auto unsorted = getUnsortedArray(1000);
+    int size = array.size();
 
+    for (int i = 1; i < size; i++)
+    {
+        int element = array[i];
+        int fromIndex = i;
+        int toIndex = i;
+
+        while (toIndex >= 1 && element < array[toIndex - 1])
+        {
+            toIndex--;
+        }
+
+        if (fromIndex != toIndex)
+        {
+            auto from = array.begin() + fromIndex;
+            auto to = array.begin() + toIndex;
+            rotate(to, from, from + 1);
+        }
+    }
+
+    return array;
+}
+
+void sort(vector<int> array,
+          vector<int> sorting(vector<int> array),
+          string name)
+{
     auto start = chrono::high_resolution_clock::now();
-    auto sorted = bubbleSort(unsorted);
+    auto sorted = sorting(array);
     auto finish = chrono::high_resolution_clock::now();
 
-    cout << "Bubble sort took: " << (finish - start).count() / 1000 << endl;
+    cout << name << " took: " << (finish - start).count() / 1000 << endl;
 
     for (int i = 0; i < sorted.size() - 1; i++)
     {
         assert(sorted[i] <= sorted[i + 1]);
     }
+}
+
+int main()
+{
+    auto unsorted = getUnsortedArray(1000);
+
+    sort(unsorted, bubbleSort, "bubbleSort");
+
+    sort(unsorted, insertionSort, "insertionSort");
 }
