3921a456b88fb0cf469c5dae17a86f76ef31f5f1
Ivan Rybalko <ivribalko@gmail.com>
2021-01-10 01:12:32 -0500

    bubble sort

--- main.cpp
+++ main.cpp
@@ -1,26 +1,60 @@
+#include <chrono>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-vector<int> getRandomNumbers(int count)
+vector<int> getUnsortedArray(int count)
 {
-    vector<int> numbers;
+    vector<int> array;
 
     for (int i = 0; i < count; i++)
     {
-        numbers.push_back(i);
+        array.push_back(i);
     }
 
     for (int i = 0; i < count; i++)
     {
-        swap(numbers[i], numbers[rand() % count]);
+        swap(array[i], array[rand() % count]);
     }
 
-    return numbers;
+    return array;
+}
+
+vector<int> bubbleSort(vector<int> array)
+{
+    int swappedCountBefore = 0;
+    int swappedCountAfter = 0;
+    int size = array.size();
+    do
+    {
+        swappedCountAfter = swappedCountBefore;
+
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap(array[i], array[i + 1]);
+                swappedCountAfter++;
+            }
+        }
+    } while (swappedCountBefore != swappedCountAfter);
+
+    return array;
 }
 
 int main()
 {
-    vector<int> numbers = getRandomNumbers(1000);
+    auto unsorted = getUnsortedArray(1000);
+
+    auto start = chrono::high_resolution_clock::now();
+    auto sorted = bubbleSort(unsorted);
+    auto finish = chrono::high_resolution_clock::now();
+
+    cout << "Bubble sort took: " << (finish - start).count() / 1000 << endl;
+
+    for (int i = 0; i < sorted.size() - 1; i++)
+    {
+        assert(sorted[i] <= sorted[i + 1]);
+    }
 }
